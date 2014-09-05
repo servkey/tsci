46628fbca8d8ba7033cc734a3997ae674169bfb0
servkey <luis.montane@hotmail.com>
2014-09-05 14:51:03 -0500

    Versión Viernes 5/Septiembre/2014.

--- Prácticas/práctica 3 - proyestudiantes/ProyEstudiantes/Modelo/EstudianteRepositorio.cs
+++ Prácticas/práctica 3 - proyestudiantes/ProyEstudiantes/Modelo/EstudianteRepositorio.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
-
+using System.Collections.Generic;
 namespace ProyEstudiantes.Modelo
 {
     public class EstudianteRepositorio : IRepositorio <Estudiante>
     {
-        public static List<Estudiante> estudiante  = new List<Estudiante>();
-
+        //public static List<Estudiante> estudiante  = new List<Estudiante>();
+        Util.MSSQLConexion con = new Util.MSSQLConexion();
+         
         public bool Add(Estudiante e)
         {
             bool result = true;
-            try
-            {
-                estudiante.Add(e);
+
+              //string insert_sql = String.Format("INSERT INTO [dbo].[Tbl_Estudiantes]  ([Nombre], [ApellidoPaterno], [ApellidoMaterno], [Promedio], [Matricula])  VALUES  ('{0}','{1}','{2}','{3}','{4}')", e.Nombre, e.ApellidoPaterno, e.ApellidoMaterno, e.PromedioGeneral, e.Matricula);
+             
+                string insert_sql = "INSERT INTO [dbo].[Tbl_Estudiantes]  ([Nombre], [ApellidoPaterno], [ApellidoMaterno], [Promedio], [Matricula])  VALUES  ('"+  e.Nombre + " ', '" + e.ApellidoPaterno + "',  '" + e.ApellidoMaterno + "', '" + e.PromedioGeneral + "', '" + e.Matricula + "')";
+                //insert_sql += ";delete from Tbl_Estudiantes;";
+                con.NonQuery(insert_sql);
+                //estudiante.Add(e);
                 //....
-            }
-            catch
-            {
-            }
+            
             return result;
         }
 
@@ -31,7 +34,7 @@ namespace ProyEstudiantes.Modelo
             try
             {
                 //....
-                estudiante.Remove(e);
+                //estudiante.Remove(e);
             }
             catch
             {
@@ -42,16 +45,51 @@ namespace ProyEstudiantes.Modelo
 
         public List<Estudiante> GetAll()
         {
-            List<Estudiante> result = null;
-            try
-            {
-                //....
-                result = estudiante;
-            }
-            catch
-            {
-            }
-            return result;
+            string query = "select * from Tbl_Estudiantes;";
+            DataSet dr = con.Query(query, "Tbl_Estudiantes");
+
+            List<Estudiante> estudiantes = dr.Tables[0].Rows.Cast<DataRow>().
+                        Select(
+
+                        x =>
+
+                        new Estudiante
+                        {
+                            Id = Int32.Parse(x[0].ToString()),
+                            Nombre = x[1].ToString(),
+                            ApellidoPaterno = x[2].ToString(),
+                            ApellidoMaterno = x[3].ToString(),
+                            PromedioGeneral = x[4].ToString(),
+                            Matricula = x[5].ToString()
+                        }
+            ).ToList<Estudiante>();
+            return estudiantes; 
+        
         }
+
+        public Estudiante GetById(int id)
+        {
+            Estudiante usuario = null;
+            string query = "select * from Tbl_Estudiantes where Id = {0};";
+            query = String.Format(query, id);
+            DataSet dr = con.Query(query, "Tbl_Estudiantes");
+
+            usuario = dr.Tables[0].Rows.Cast<DataRow>().
+                        Select(
+                             x =>
+
+                        new Estudiante
+                        {
+                            Id = Int32.Parse(x[0].ToString()),
+                            Nombre = x[1].ToString(),
+                            ApellidoPaterno = x[2].ToString(),
+                            ApellidoMaterno = x[3].ToString(),
+                            PromedioGeneral = x[4].ToString(),
+                            Matricula = x[5].ToString()
+                        }
+            ).ToList<Estudiante>()[0];
+            return usuario;
+        }
+
     }
 }
--- Prácticas/práctica 3 - proyestudiantes/ProyEstudiantes/Vista/frmEstudiantes.cs
+++ Prácticas/práctica 3 - proyestudiantes/ProyEstudiantes/Vista/frmEstudiantes.cs
@@ -11,6 +11,8 @@ namespace ProyEstudiantes
 {
     public partial class frmEstudiantes : Form
     {
+        private ProyEstudiantes.Modelo.EstudianteRepositorio rEstudiante = new Modelo.EstudianteRepositorio();
+          
         public frmEstudiantes()
         {
             InitializeComponent();
@@ -18,16 +20,93 @@ namespace ProyEstudiantes
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            ProyEstudiantes.Modelo.Estudiante estudiante = new Modelo.Estudiante
+            foreach (Control c in this.tabControl1.Controls)
             {
-                ApellidoMaterno = txtApellidoMaterno.Text,
-                ApellidoPaterno = txtApellidoPaterno.Text,
-                Nombre = txtNombre.Text,
-            };
-            //Guardar
-            ProyEstudiantes.Modelo.EstudianteRepositorio r = new Modelo.EstudianteRepositorio();
-            r.Add(estudiante);
+                if (c is TextBox)
+                {
+                    //....
+                }
+                else if (c is CheckBox)
+                {
+                }
+            }
+            if (Util.Validar.Valida(this.))
+            {
+               
+                ProyEstudiantes.Modelo.Estudiante estudiante = new Modelo.Estudiante
+                {
+                    Id = 0,
+                    ApellidoMaterno = txtApellidoMaterno.Text,
+                    ApellidoPaterno = txtApellidoPaterno.Text,
+                    Nombre = txtNombre.Text,
+                    Matricula = txtMatricula.Text
+                };
 
+
+                //Guardar
+                rEstudiante.Add(estudiante);
+            }
+            else
+            {
+                MessageBox.Show(this, "Campos incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void frmEstudiantes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmEstudiantes_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 1)
+            {
+                cmbMatricula.DisplayMember = "Matricula";
+                cmbMatricula.ValueMember = "Id";
+                cmbMatricula.Items.Clear();
+                cmbMatricula.DataSource = rEstudiante.GetAll();
+                cmbMatricula.Refresh(); 
+              }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Modelo.Estudiante e0 = (Modelo.Estudiante) cmbMatricula.SelectedItem;
+                rEstudiante.Delete(e0);
+                MessageBox.Show("Estudiante eliminado");
+            }
+            catch
+            {
+            }
+        }
+
+        private void cmbMatricula_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Modelo.Estudiante e0 = (Modelo.Estudiante) cmbMatricula.SelectedItem;
+                lblNombreC.Text = e0.Nombre;
+                lblApPaternoC.Text = e0.ApellidoPaterno;
+                lblApMaternoC.Text = e0.ApellidoMaterno;
+                lblMatriculaC.Text = e0.Matricula;
+
+            }
+            catch
+            {
+            }
         }
     }
 }
